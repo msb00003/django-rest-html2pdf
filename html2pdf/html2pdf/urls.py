a073b81abbf06a0256c11cc1a0f8da6c6b9648b0
EC2 Default User <ec2-user@ip-172-31-26-123.eu-west-1.compute.internal>
2016-01-16 13:09:20 +0000

    YAY WORKS
    Committer: msb00003 <msb00003@gmail.com>

--- html2pdf/html2pdf/urls.py
+++ html2pdf/html2pdf/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from converter import views
 
 urlpatterns = [
-    url(r'^html2pdf/', include('converter.urls')),
+    url(r'^html2pdf/(?P<json>.+)/$', views.index), 	
     url(r'^admin/', admin.site.urls),
 ]
